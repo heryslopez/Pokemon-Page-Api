9fbae9b7d2732f4e6d3754e07c5021d648d9671c
herys lopez <heryslopez1999@gmail.com>
2022-09-25 19:22:57 -0600

    upload file local proyect

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { ViewComponent } from './components/view/view.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    ViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
